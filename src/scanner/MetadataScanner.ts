e9a88acb2c1921ca5a6ef78c4798e796e02c0c74
Dancho <131505798+Dancho1009@users.noreply.github.com>
2026-09-20 03:35:06 +0800

    feat: 扩展本地歌词和标签扫描信息

--- src/scanner/MetadataScanner.ts
+++ src/scanner/MetadataScanner.ts
@@ -1,28 +1,38 @@
 import { parseFile } from "music-metadata";
+import { LyricsScanner, LyricsInfo } from "./LyricsScanner";
 
 export interface AudioMetadata {
   path: string;
   title?: string;
   artist?: string;
   album?: string;
+  albumArtist?: string;
+  composer?: string;
   duration?: number;
   year?: number;
   genre?: string;
   isrc?: string;
   cover?: string;
+  lyrics?: LyricsInfo;
 }
 
 export class MetadataScanner {
+  private lyricsScanner = new LyricsScanner();
+
   async scanFile(filePath: string): Promise<AudioMetadata> {
     const metadata = await parseFile(filePath);
     const common = metadata.common;
     const format = metadata.format;
+
+    const lyrics = await this.lyricsScanner.scan(filePath, metadata);
 
     return {
       path: filePath,
       title: common.title,
       artist: common.artist,
       album: common.album,
+      albumArtist: common.albumartist,
+      composer: common.composer?.[0],
       duration: format.duration,
       year: common.year,
       genre: common.genre?.[0],
@@ -30,6 +40,7 @@ export class MetadataScanner {
       cover: common.picture?.[0]
         ? `embedded:${common.picture[0].format}`
         : undefined,
+      lyrics,
     };
   }
 }
