61bba23650b7d2ac423fa997e9c4247ae5da1376
Dancho <131505798+Dancho1009@users.noreply.github.com>
2026-09-20 05:20:46 +0800

    feat: 暴露扫描进度监听接口

--- src/main/preload.ts
+++ src/main/preload.ts
@@ -3,6 +3,9 @@ import { contextBridge, ipcRenderer } from "electron";
 contextBridge.exposeInMainWorld("appleMetaFix", {
   selectFolder: () => ipcRenderer.invoke("select-folder"),
   scanFolder: (folderPath: string) => ipcRenderer.invoke("scan-folder", folderPath),
+  onScanProgress: (callback: (progress: any) => void) => {
+    ipcRenderer.on("scan-progress", (_event, progress) => callback(progress));
+  },
   openFileLocation: (filePath: string) => ipcRenderer.invoke("open-file-location", filePath),
   openLyricsFile: (lyricsPath?: string) => ipcRenderer.invoke("open-lyrics-file", lyricsPath),
 });
