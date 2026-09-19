4265b94cc890b88ae429ea800200b77558edacd3
Dancho <131505798+Dancho1009@users.noreply.github.com>
2026-09-20 02:10:02 +0800

    实现真实音乐元数据扫描模块

--- src/scanner/MetadataScanner.ts
+++ src/scanner/MetadataScanner.ts
@@ -1,3 +1,5 @@
+import { parseFile } from "music-metadata";
+
 export interface AudioMetadata {
   path: string;
   title?: string;
@@ -7,12 +9,27 @@ export interface AudioMetadata {
   year?: number;
   genre?: string;
   isrc?: string;
+  cover?: string;
 }
 
 export class MetadataScanner {
   async scanFile(filePath: string): Promise<AudioMetadata> {
+    const metadata = await parseFile(filePath);
+    const common = metadata.common;
+    const format = metadata.format;
+
     return {
       path: filePath,
+      title: common.title,
+      artist: common.artist,
+      album: common.album,
+      duration: format.duration,
+      year: common.year,
+      genre: common.genre?.[0],
+      isrc: common.isrc?.[0],
+      cover: common.picture?.[0]
+        ? `embedded:${common.picture[0].format}`
+        : undefined,
     };
   }
 }
