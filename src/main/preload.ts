d38cbb5ec2f3fbcb3012a265c7fea4798bd9a3db
Dancho <131505798+Dancho1009@users.noreply.github.com>
2026-09-20 03:56:05 +0800

    feat: 增加歌曲操作接口

--- src/main/preload.ts
+++ src/main/preload.ts
@@ -3,4 +3,6 @@ import { contextBridge, ipcRenderer } from "electron";
 contextBridge.exposeInMainWorld("appleMetaFix", {
   selectFolder: () => ipcRenderer.invoke("select-folder"),
   scanFolder: (folderPath: string) => ipcRenderer.invoke("scan-folder", folderPath),
+  openFileLocation: (filePath: string) => ipcRenderer.invoke("open-file-location", filePath),
+  openLyricsFile: (lyricsPath?: string) => ipcRenderer.invoke("open-lyrics-file", lyricsPath),
 });
