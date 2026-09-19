10331543e2ba03d716c522ccc2bdc2c282a21da9
Dancho <131505798+Dancho1009@users.noreply.github.com>
2026-09-20 05:33:24 +0800

    fix preload ipc listener cleanup

--- src/main/preload.ts
+++ src/main/preload.ts
@@ -7,6 +7,9 @@ contextBridge.exposeInMainWorld("appleMetaFix", {
   onScanProgress: (callback: (progress: any) => void) => {
     ipcRenderer.on("scan-progress", (_event, progress) => callback(progress));
   },
+  offScanProgress: (callback: (progress: any) => void) => {
+    ipcRenderer.removeListener("scan-progress", callback as any);
+  },
   openFileLocation: (filePath: string) => ipcRenderer.invoke("open-file-location", filePath),
   openLyricsFile: (lyricsPath?: string) => ipcRenderer.invoke("open-lyrics-file", lyricsPath),
 });
