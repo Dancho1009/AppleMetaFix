bf995e2164199c64cfba03bca6ea56a67ecca4a8
Dancho <131505798+Dancho1009@users.noreply.github.com>
2026-09-21 00:56:41 +0800

    fix: 修复本地音频技术信息读取

--- src/services/LocalMetadataReader.ts
+++ src/services/LocalMetadataReader.ts
@@ -1,10 +1,12 @@
 import { parseFile } from "music-metadata";
 import { LocalTrack } from "../models/Track";
+import { statSync } from "fs";
 
 export class LocalMetadataReader {
   async read(filePath: string): Promise<LocalTrack> {
     const metadata = await parseFile(filePath);
     const common = metadata.common;
+    const format = metadata.format;
 
     return {
       path: filePath,
@@ -12,7 +14,12 @@ export class LocalMetadataReader {
       artist: common.artist,
       album: common.album,
       year: common.year,
-      durationInSeconds: metadata.format.duration
+      durationInSeconds: format.duration,
+      bitrate: format.bitrate,
+      sampleRate: format.sampleRate,
+      bitDepth: format.bitsPerSample,
+      size: statSync(filePath).size,
+      format: format.container
     };
   }
 }
